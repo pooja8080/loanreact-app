d9a73f091b737c37d0564d50b08ee5ebccd14f27
poojasri <panigrahi.pooja-s@metlife.com>
2019-05-15 15:09:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import InputRange from "react-input-range";
 import './App.css';
+import Display from "./Display";
 
-class App extends Component {
+import "react-input-range/lib/css/index.css";
+
+
+class App extends React.Component {
+  state = {
+    amountValue: 5000,
+    yearsValue: 1
+  };
+
+  handleAmountChange = value => {
+    this.setState({ amountValue: value });
+  };
+  handleYearChange = value => {
+    this.setState({ yearsValue: value });
+  };
+
   render() {
+    let { amountValue, yearsValue } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h4>I want to borrow ${amountValue}</h4>
+        <InputRange
+          step={100}
+          maxValue={20000}
+          minValue={1000}
+          value={amountValue}
+          onChange={this.handleAmountChange}
+        />
+        <h4>
+          Over {yearsValue} year{yearsValue > 1 && "s"}
+        </h4>
+        <InputRange
+          step={0.5}
+          maxValue={5}
+          minValue={1}
+          value={yearsValue}
+          onChange={this.handleYearChange}
+        />
+        <Display years={yearsValue} amount={amountValue} />
       </div>
     );
   }
 }
 
-export default App;
+    // Renders the App component into a div with id 'root'
+   export default App;
